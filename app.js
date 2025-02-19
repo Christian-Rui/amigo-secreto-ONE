77cdbb740a7f3fa8d05be0e735d70ddf68854455
Christian-Rui <chris2w0social@gmail.com>
2025-02-19 00:02:36 -0300

    feat: adicionar nomes ao array e focar no input

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomesAmigosArray = [];
+
+let input = document.getElementById("amigo");
+
+function inicializarConfig() {
+    focarNoInput()
+}
+
+function focarNoInput() {
+  document.addEventListener("DOMContentLoaded", function () {
+    input.focus();
+  });
+}
+
+inicializarConfig();
+
+function adicionarAmigo() {
+  if (input.value === "") {
+    alert("Você não digitou nenhum nome, tente novamente!");
+  } else {
+    nomesAmigosArray.push(input.value);
+    input.value = "";
+  }
+}
