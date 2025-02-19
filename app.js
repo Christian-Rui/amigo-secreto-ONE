502d2a23e31affaffcd7480c3a0bea7efeaaf002
Christian-Rui <chris2w0social@gmail.com>
2025-02-19 01:45:31 -0300

    docs: adicionar JsDocs as functions do projeto

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ let nomesAmigosArray = [];
 let input = document.getElementById("amigo");
 
 let podeAlertar = {
-    adicionar: true,
-    sortear: true
+  adicionar: true,
+  sortear: true,
 };
 
+/**
+ * Inicializa a configuração do aplicativo
+ */
 function inicializarConfig() {
   focarNoInput();
 }
 
+/**
+ * Foca no campo de entrada quando o documento é carregado.
+ */
 function focarNoInput() {
   document.addEventListener("DOMContentLoaded", function () {
     input.focus();
@@ -21,21 +27,31 @@ function focarNoInput() {
 
 inicializarConfig();
 
+/**
+ * Converte a primeira letra de uma string para maiúscula.
+ * @param {string} ValorString - A string a ser convertida.
+ * @returns {string} A string com a primeira letra em maiúscula.
+ */
 function iniciarComLetraMaiuscula(ValorString = "") {
   return ValorString.charAt(0).toUpperCase() + ValorString.substring(1);
 }
 
+/**
+ * Adiciona um amigo ao array de amigos, com validações para entradas vazias e duplicadas.
+ */
 function adicionarAmigo() {
   let idAlerta = "alertaInput";
   let chave = "adicionar";
   if (input.value === "") {
     alertarAusenciaDeValor(
-      idAlerta, chave,
+      idAlerta,
+      chave,
       "Você não digitou nenhum nome, tente novamente!"
     );
   } else if (nomesAmigosArray.includes(input.value)) {
     alertarAusenciaDeValor(
-      idAlerta, chave,
+      idAlerta,
+      chave,
       `O nome '${iniciarComLetraMaiuscula(
         input.value
       )}' já foi adicionado, tente outro nome`
@@ -50,6 +66,12 @@ function adicionarAmigo() {
   }
 }
 
+/**
+ * Renderiza a lista de amigos na interface.
+ * @param {string} id - O ID do elemento onde a lista será renderizada.
+ * @param {Array|string} valor - Os valores a serem renderizados na lista.
+ * @param {string} [textoOpcional=""] - Texto opcional a ser adicionado antes de cada item da lista.
+ */
 function renderizarListaValores(id, valor, textoOpcional = "") {
   let lista = document.getElementById(id);
   let max = Array.isArray(valor) ? valor.length : 1;
@@ -61,6 +83,9 @@ function renderizarListaValores(id, valor, textoOpcional = "") {
   }
 }
 
+/**
+ * Sorteia um amigo do array de amigos e exibe o resultado.
+ */
 function sortearAmigo() {
   let tamanhoMax = nomesAmigosArray.length;
   let nomeSorteado = nomesAmigosArray[gerarRandomIndex(tamanhoMax)];
@@ -77,30 +102,43 @@ function sortearAmigo() {
     );
   } else {
     alertarAusenciaDeValor(
-      idAlerta, chave,
-      "⚠Por favor, adicione mais que 1 nome para o sorteio"
+      idAlerta,
+      chave,
+      "Por favor, adicione mais que 1 nome para o sorteio"
     );
   }
 }
 
+/**
+ * Gera um índice aleatório entre 0 e o valor máximo fornecido.
+ * @param {number} max - O valor máximo para a geração do índice.
+ * @returns {number} Um índice aleatório.
+ */
 function gerarRandomIndex(max) {
   return Math.floor(Math.random() * max);
 }
 
+/**
+ * Exibe uma mensagem de alerta na interface, com controle de exibição baseado em uma chave.
+ * ---
+ * OBS: caso nenhum texto seja adicionado, ele apenas apagará qualquer texto que existir
+ * @param {string} id - O ID do elemento onde a mensagem será exibida.
+ * @param {string} chave - A chave que controla se o alerta pode ser exibido.
+ * @param {string} [texto=""] - O texto opcional a ser exibido no alerta.
+ */
 function alertarAusenciaDeValor(id, chave, texto = "") {
   let pAlerta = document.getElementById(id);
   if (podeAlertar[chave]) {
     pAlerta.innerHTML = "";
     pAlerta.classList.remove("hide");
     if (texto !== "") {
-      pAlerta.innerHTML = texto;
+      pAlerta.innerHTML = `⚠ ${texto}`;
       pAlerta.classList.add("show");
       podeAlertar[chave] = false;
       setTimeout(() => {
         pAlerta.classList.remove("show");
         pAlerta.classList.add("hide");
         podeAlertar[chave] = true;
-        console.log("Teste");
       }, 3000);
     }
   }
